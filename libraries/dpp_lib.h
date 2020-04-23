938a7c6843eeaca1878a7c33fbee93ece34cdcc2
Reto Da Forno <reto.daforno@tik.ee.ethz.ch>
2020-04-23 17:18:12 +0200

    fifo lib function added

--- libraries/dpp_lib.h
+++ libraries/dpp_lib.h
@@ -8,6 +8,46 @@
 #ifndef __DPP_LIB_H__
 #define __DPP_LIB_H__
 
+/* includes */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+/* config */
+
+#ifndef FIFO_STATIC_ALLOC
+#define FIFO_STATIC_ALLOC         1         /* if static allocation is used, use the macro FIFO_CREATE() instead of the function fifo_create() */
+#endif /* FIFO_STATIC_ALLOC */
+
+
+/* macros */
+
+#if FIFO_STATIC_ALLOC
+#define FIFO_CREATE(fifo_name, element_size, num_elements)  static uint8_t fifo_name##_buffer[element_size * num_elements + sizeof(fifo_meta_t)]; \
+                                                            static fifo_t fifo_name = fifo_name##_buffer; \
+                                                            ((fifo_meta_t*)fifo_name)->write_idx = 0; \
+                                                            ((fifo_meta_t*)fifo_name)->read_idx = 0; \
+                                                            ((fifo_meta_t*)fifo_name)->elem_size = element_size; \
+                                                            ((fifo_meta_t*)fifo_name)->num_elem = num_elements
+#else
+#define FIFO_CREATE(fifo_name, element_size, num_elements)  fifo_t fifo_name = fifo_create(element_size, num_elements)
+#endif /* FIFO_STATIC_ALLOC */
+
+
+/* typedefs */
+
+typedef void* fifo_t;
+
+// size must match FIFO_META_SIZE!
+typedef struct fifo {
+  uint32_t write_idx;
+  uint32_t read_idx;
+  uint32_t elem_size;
+  uint32_t num_elem;
+} fifo_meta_t;
+
 
 /* function prototypes */
 
@@ -15,6 +55,18 @@ uint16_t crc16(const uint8_t* data, uint8_t num_bytes, uint16_t init_value);
 
 uint32_t hexstr_to_uint32(const char* str);
 uint32_t uint16_to_str(uint16_t val, char* out_buffer);
+
+#if !FIFO_STATIC_ALLOC
+fifo_t fifo_create(uint32_t elem_size, uint32_t num_elem);
+#endif /* FIFO_STATIC_ALLOC */
+void fifo_clear(fifo_t fifo);
+bool fifo_push(fifo_t fifo, const void* elem);
+bool fifo_pop(fifo_t fifo, void* elem);
+const void* const fifo_peek(const fifo_t fifo);
+bool fifo_is_empty(const fifo_t fifo);
+bool fifo_is_full(const fifo_t fifo);
+uint32_t fifo_get_used_space(const fifo_t fifo);
+uint32_t fifo_get_free_space(const fifo_t fifo);
 
 
 #endif /* __DPP_LIB_H__ */
---
+++ libraries/fifo.c
@@ -0,0 +1,114 @@
+/*
+ * fifo.c
+ * a simple and lightweight ring buffer / FIFO queue
+ *
+ *  Created on: Apr 23, 2020
+ *      Author: rdaforno
+ */
+
+#include "dpp_lib.h"
+
+
+#define FIFO_GET_ELEM_BY_INDEX(idx)   (void*)((uint32_t)fifo + sizeof(fifo_meta_t) + (idx * ((fifo_meta_t*)fifo)->elem_size))
+
+/*---------------------------------------------------------------------------*/
+#if !FIFO_STATIC_ALLOC
+fifo_t fifo_create(uint32_t elem_size, uint32_t num_elem)
+{
+  if (num_elem == 0 || num_elem == 0) {
+    return NULL;
+  }
+  fifo_t fifo = malloc(elem_size * num_elem + sizeof(fifo_meta_t));
+  if (!fifo) {
+    return NULL;
+  }
+  fifo_meta_t* f = (fifo_meta_t*)fifo;
+  f->write_idx = 0;
+  f->read_idx = 0;
+  f->elem_size = elem_size;
+  f->num_elem = num_elem;
+
+  return fifo;
+}
+#endif /* FIFO_STATIC_ALLOC */
+/*---------------------------------------------------------------------------*/
+void fifo_clear(fifo_t fifo)
+{
+  if (fifo) {
+    ((fifo_meta_t*)fifo)->write_idx = 0;
+    ((fifo_meta_t*)fifo)->read_idx = 0;
+  }
+}
+/*---------------------------------------------------------------------------*/
+bool fifo_is_empty(const fifo_t fifo)
+{
+  if (fifo) {
+    return (((fifo_meta_t*)fifo)->read_idx == ((fifo_meta_t*)fifo)->write_idx);
+  }
+  return true;
+}
+/*---------------------------------------------------------------------------*/
+bool fifo_is_full(const fifo_t fifo)
+{
+  if (fifo) {
+    fifo_meta_t* f = (fifo_meta_t*)fifo;
+    uint32_t w = ((f->write_idx + 1) == f->num_elem) ? 0 : (f->write_idx + 1);
+    return (w == f->read_idx);
+  }
+  return true;
+}
+/*---------------------------------------------------------------------------*/
+/* get current fill level */
+uint32_t fifo_get_used_space(const fifo_t fifo)
+{
+  if (fifo) {
+    fifo_meta_t* f = (fifo_meta_t*)fifo;
+    if (f->write_idx < f->read_idx) {
+      return (f->num_elem - f->read_idx + f->write_idx);
+    }
+    return (f->write_idx - f->read_idx);
+  }
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+uint32_t fifo_get_free_space(const fifo_t fifo)
+{
+  return (((fifo_meta_t*)fifo)->num_elem - fifo_get_used_space(fifo));
+}
+/*---------------------------------------------------------------------------*/
+bool fifo_push(fifo_t fifo, const void* elem)
+{
+  if (!fifo || !elem || fifo_is_full(fifo)) {
+    return false;
+  }
+  fifo_meta_t* f = (fifo_meta_t*)fifo;
+  memcpy(FIFO_GET_ELEM_BY_INDEX(f->write_idx), elem, f->elem_size);
+  f->write_idx++;
+  if (f->write_idx == f->num_elem) {
+    f->write_idx = 0;
+  }
+  return false;
+}
+/*---------------------------------------------------------------------------*/
+bool fifo_pop(fifo_t fifo, void* elem)
+{
+  if (!fifo || !elem || !fifo_is_empty(fifo)) {
+    return false;
+  }
+  fifo_meta_t* f = (fifo_meta_t*)fifo;
+  memcpy(elem, FIFO_GET_ELEM_BY_INDEX(f->read_idx), f->elem_size);
+  ((fifo_meta_t*)fifo)->read_idx++;
+  if (f->read_idx == f->num_elem) {
+    f->read_idx = 0;
+  }
+  return true;
+}
+/*---------------------------------------------------------------------------*/
+const void* const fifo_peek(const fifo_t fifo)
+{
+  if (fifo && !fifo_is_empty(fifo)) {
+    return FIFO_GET_ELEM_BY_INDEX(((fifo_meta_t*)fifo)->read_idx);
+  }
+  return NULL;
+}
+/*---------------------------------------------------------------------------*/
