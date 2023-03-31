91033071bfaa2fbe486bf00883f0164ff9d1d192
Reto Da Forno <reto.daforno@tik.ee.ethz.ch>
2023-03-31 18:09:01 +0200

    remove obsolete check

--- definitions/messages/dpp_message.h
+++ definitions/messages/dpp_message.h
@@ -370,11 +370,6 @@ typedef struct {
  * E R R O R   C H E C K S
  */
 
-#if (DPP_MSG_PAYLOAD_LEN < DPP_MSG_COM_HEALTH_LEN) || \
-    (DPP_MSG_PAYLOAD_LEN < DPP_MSG_NODE_INFO_LEN)
-  #error "payload of message_t is too big"
-#endif
-
 #ifdef BOLT_CONF_MAX_MSG_LEN
   #if BOLT_CONF_MAX_MSG_LEN < MSG_PKT_LEN
     #error "BOLT max msg length is too small"
